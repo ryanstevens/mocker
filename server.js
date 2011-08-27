c9cc905c4cef956efdd64350623073881459cec3
stevens.ryan@gmail.com <ryanstevens@Ryan-Stevenss-MacBook-Pro.local>
2011-08-26 20:07:35 -0700

    first check @ JOYENT!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http')
 
 var app = http.createServer(function (req, res) { 
     res.writeHead(200, { 'Content-Type': 'text/html' }); 
-    res.end('Hello, my name is HAL'); 
+    res.end('Hello, my name is HAL.  Joyent HQ is pretty legit'); 
 });
 
 app.listen(parseInt(process.env.PORT) || 7777); 
