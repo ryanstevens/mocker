3a375dd626028b0d2e766056404a7f69885db52c
stevens.ryan@gmail.com <ryanstevens@Ryan-Stevenss-MacBook-Pro.local>
2011-08-27 14:36:39 -0700

    added socket.io

--- public/js/client/browserver.js
+++ public/js/client/browserver.js
@@ -1,14 +1,22 @@
-var bsr = {};
-
-
+//default namespace
+var mok = {
+    who : uuid()
+};
 
 function init() {
-    
-      var editor = CodeMirror.fromTextArea($('.code-block textarea')[0], {
+    var editor = CodeMirror.fromTextArea($('.code-block textarea')[0], {
         lineNumbers: true,
         matchBrackets: true
-      });
+    });
     
+    mok.socket = io.connect('/');
+  
+    mok.socket.on('challenge', function() {
+        mok.socket.emit('challenge-response', mok.who);
+    });
 
+    mok.socket.on('fetch', function(req) {
+        console.log(req);
+    });
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http') 
     , nko = require('nko')('zYF4TnFYVYG9ggWG')
-    , express = require('express');
+    , express = require('express')
+    , mocker = require('./lib/Mocker.js');
 
 
 
@@ -10,19 +11,20 @@ app.set('view engine', 'jade');
 app.set('log level', 1);
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-    res.render('index.jade', { pageTitle: 'BrowServer' });
+    res.render('index.jade');
 });
 
+app.get('/server/:uuid/*', function(req, res) {
+    console.log(req.params.uuid);
+    mocker.handleResponse(req, res, req.params.uuid);
+});
 app.listen(parseInt(process.env.PORT) || 7777);
 
 
 var io = require('socket.io').listen(app);
-
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  
+    mocker.registerClient(socket, app);
+    
 });
-
 console.log('Listening on ' + app.address().port);
