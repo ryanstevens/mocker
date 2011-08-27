9d575e462107e1ff72d6558031909d538edfae4c
stevens.ryan@gmail.com <ryanstevens@Ryan-Stevenss-MacBook-Pro.local>
2011-08-27 09:03:27 -0700

    Adding dependancies, configuring

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var http = require('http') 
-, nko = require('nko')('zYF4TnFYVYG9ggWG');
+    , nko = require('nko')('zYF4TnFYVYG9ggWG')
+    , express = require('express');
 
-var app = http.createServer(function (req, res) { 
-    res.writeHead(200, { 'Content-Type': 'text/html' }); 
-    res.end('Hello, my name is HAL.  Joyent HQ is pretty legit'); 
+
+
+
+var app = express.createServer();
+app.set('view engine', 'jade');
+app.set('log level', 1);
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+    res.render('index.jade', { pageTitle: 'BrowServer' });
 });
 
-app.listen(parseInt(process.env.PORT) || 7777); 
+app.listen(parseInt(process.env.PORT) || 7777);
+
+
 console.log('Listening on ' + app.address().port);
